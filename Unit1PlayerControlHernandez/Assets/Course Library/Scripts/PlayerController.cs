a8dd1b27755fac062a4bc51f9a2a7413ded164f2
747058 <747058@student.dusd.net>
2023-03-01 10:48:41 -0800

    recorded and finished game

--- Unit1PlayerControlHernandez/Assets/Course Library/Scripts/PlayerController.cs
+++ Unit1PlayerControlHernandez/Assets/Course Library/Scripts/PlayerController.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
-public class traffic : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    
-    
-    // Start is called before the first frame update
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+     // Start is called before the first frame update
     void Start()
     {
         
@@ -16,7 +20,17 @@ public class traffic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move the vehicle forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Move the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        //Rotates the car based on the hoprixontal input 
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+
     }
 }
--- Unit1PlayerControlHernandez/Assets/Course Library/Scripts/traffic.cs
+++ Unit1PlayerControlHernandez/Assets/Course Library/Scripts/traffic.cs
@@ -1,28 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class traffic : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 90.0f;
-    private float horizontalInput;
-    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        // Moves the car forward based on vertical inp[ut
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        // Rotates the car based on the horizontal Input
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.forward * Time.deltaTime * 15);
     }
 }
